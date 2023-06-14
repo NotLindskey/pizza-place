a3032d0155f4892e25b6d5359c6495c5bcf7eea5
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-13 20:04:28 -0500

    created header component into nav links and added routes

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1 class="site_title">
+      <routerLink to="/">~ Pizza Planet ~</routerLink>
+    </h1>
+    <nav>
+      <RouterLink class="link" to="/">Home</RouterLink>
+      <RouterLink class="menu" to="/menu">Menu</RouterLink>
+      <RouterLink class="About" to="/about">About</RouterLink>
+      <RouterLink class="Admin" to="/admin">Admin</RouterLink>
+    </nav>
+    <h2>The #1 Place For Pizza</h2>
+  </header>
+</template>
