e2e2337f43896ac7a638c4ea926ef0cb683d623a
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-18 13:33:33 -0500

    The about view and child components, added routes to nav

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,27 @@
 <template>
-  <h3>about</h3>
+  <div class="about">
+    <section class="intro">
+      <div class="info_text_wrapper"></div>
+      <h3>Quality Pizza for 30 years</h3>
+      <p class="info_text">
+        Nothing but the best of quality ingredients, with a friendly atmosphere.
+      </p>
+      <img
+        class="info_img"
+        src="../assets/pizza-planet-v3-images-main/images/calzone.jpg"
+        alt="calzone close up image"
+      />
+    </section>
+    <section class="more_info_wrapper">
+      <h3>Click on the links below for more info</h3>
+      <nav>
+        <ul>
+          <RouterLink class="link" :to="{ name: 'historyLink' }">Our History</RouterLink>
+          <RouterLink class="link" :to="{ name: 'deliveryLink' }">Delivery</RouterLink>
+          <RouterLink class="link" :to="{ name: 'locationsLink' }">Locations</RouterLink>
+        </ul>
+      </nav>
+      <RouterView />
+    </section>
+  </div>
 </template>
