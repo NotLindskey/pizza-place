ca1eca0391a955d7fe6286a797d72baf2c9e43ac
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-18 15:37:00 -0500

    created home component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,52 @@
 <script setup></script>
 
 <template>
-  <main>
-    <h3>Home</h3>
+  <main class="hot">
+    <div class="hot_text">
+      <h3>Hot out the Oven!</h3>
+      <p class="hot_pizza_name">Vegetarian Hot Pizza</p>
+      <p class="hot_pizza_description">Our signature vegetarian, but with a kick...</p>
+      <RouterLink :to="{ name: 'menuLink' }">Let's Order</RouterLink>
+    </div>
+    <img
+      class="hot_img"
+      src="../assets/pizza-planet-v3-images-main/images/fire.jpg"
+      alt="Pizza oven image
+      "
+    />
   </main>
 </template>
+
+<style scoped>
+.hot {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.hot_img,
+.hot_text {
+  flex: 1;
+  padding: 2rems;
+  text-align: center;
+}
+
+.hot_text h3 {
+  color: #bb2118;
+}
+
+.hot_pizza {
+  font-size: 1.8rem;
+}
+
+.hot_pizza_description {
+  font-style: italic;
+  font-weight: 300;
+}
+
+@media (min-width: 900px) {
+  .hot {
+    flex-direction: row;
+  }
+}
+</style>
