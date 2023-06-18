20dfe07b369201967e9b61c6244c754d82a44b0c
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-18 14:58:49 -0500

    created Admin: Pizzas component

---
+++ src/components/admin/Pizzas.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <section class="admin_section">
+    <header class="admin_section_header">
+      <h3>Menu</h3>
+    </header>
+    <table>
+      <thead>
+        <tr>
+          <th>Pizza</th>
+          <th>Remove from menu</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Margherita</td>
+          <td>
+            <button type="button" class="bth_remove">&times;</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
