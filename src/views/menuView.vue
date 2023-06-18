ef2ba3a979542f57e5f376426d0d62581116faf7
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-18 18:00:59 -0500

    placed functions into the composables folder

--- src/views/menuView.vue
+++ src/views/menuView.vue
@@ -1,6 +1,9 @@
 <script setup>
-import { ref, computed } from 'vue'
-const basket = ref([])
+import { ref } from 'vue'
+import useBasket from '@/composables/useBasket'
+
+const { basket, increaseQuantity, decreaseQuantity, addToBasket, total } = useBasket()
+
 const allPizzas = ref([
   {
     name: 'Margarita',
@@ -19,45 +22,6 @@ const allPizzas = ref([
     ]
   }
 ])
-
-function addToBasket(item, option) {
-  const pizzaExists = basket.value.find(function (pizza) {
-    return pizza.name === item.name && pizza.size === option.size
-  })
-  if (pizzaExists) {
-    pizzaExists.quantity++
-    return
-  }
-  basket.value.push({
-    name: item.name,
-    price: option.price,
-    size: option.size,
-    quantity: 1
-  })
-}
-
-function increaseQuantity(item) {
-  item.quantity++
-}
-
-function decreaseQuantity(item) {
-  item.quantity--
-  if (item.quantity === 0) {
-    removeFromBasket(item)
-  }
-}
-
-function removeFromBasket(item) {
-  basket.value.splice(basket.value.indexOf(item), 1)
-}
-
-const total = computed(function () {
-  let totalCost = 0
-  basket.value.forEach(function (item) {
-    totalCost += item.quantity * item.price
-  })
-  return totalCost
-})
 </script>
 
 <template>
