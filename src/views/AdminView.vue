9a9b9c51b8e790d4611450f7570bf0516a4551d3
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-18 14:58:56 -0500

    imported pizzas

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import NewPizza from '@/components/admin/NewPizza.vue'
+import Pizzas from '@/components/admin/Pizzas.vue'
 </script>
 
 <template>
   <h3>Admin</h3>
   <NewPizza />
+  <Pizzas />
 </template>
