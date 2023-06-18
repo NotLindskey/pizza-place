65074a3df52ea2175b0c5c671862f19df6827164
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-18 14:13:13 -0500

    added fonts & base styling

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,9 @@
     </h1>
     <nav>
       <RouterLink class="link" :to="{ name: 'home' }">Home</RouterLink>
-      <RouterLink class="Menu" :to="{ name: 'menuLink' }">Menu</RouterLink>
-      <RouterLink class="About" :to="{ name: 'aboutLink' }">About</RouterLink>
-      <RouterLink class="Admin" :to="{ name: 'adminLink' }">Admin</RouterLink>
+      <RouterLink class="link" :to="{ name: 'menuLink' }">Menu</RouterLink>
+      <RouterLink class="link" :to="{ name: 'aboutLink' }">About</RouterLink>
+      <RouterLink class="link" :to="{ name: 'adminLink' }">Admin</RouterLink>
     </nav>
     <h2>The #1 Place For Pizza</h2>
   </header>
