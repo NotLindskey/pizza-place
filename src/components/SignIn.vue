c710bc41e23db2625bbc1eebef3b3ef0de85e391
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-19 12:35:59 -0500

    added styling to modal

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -36,3 +36,58 @@ const userData = ref({
     </div>
   </div>
 </template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.9);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal_content {
+  background: whitesmoke;
+  width: 80vw;
+  padding: 1rem;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  color: rgb(76, 76, 76);
+  text-shadow: none;
+}
+
+.modal_content button:first-of-type {
+  margin: 1rem;
+  background-color: rgb(163, 203, 163);
+}
+
+.modal_content button:hover {
+  transform: scale(1.03);
+}
+
+.close_modal {
+  align-self: flex-end;
+  cursor: pointer;
+}
+
+.modal_text {
+  align-self: center;
+}
+
+.error_message {
+  margin-left: 1rem;
+  color: rgb(255, 104, 104);
+}
+
+@media (min-width: 900px) {
+  .modal_content {
+    width: 40vw;
+  }
+}
+</style>
