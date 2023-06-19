f1bd54e2b2aaf041fb0203ce6d37899230a356a3
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-19 14:38:55 -0500

    exported db

--- src/firebase.js
+++ src/firebase.js
@@ -17,7 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-const db = getFirestore(app)
-
+export const db = getFirestore(app)
 export const dbPizzasRef = collection(db, 'pizzas')
 export const dbOrdersRef = collection(db, 'orders')
